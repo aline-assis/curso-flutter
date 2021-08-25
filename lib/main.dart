ce5404d342c880deb203cd1e3abbba22aaac364e
Aline <66894943+aline-assis@users.noreply.github.com>
2021-08-24 23:55:14 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,57 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() {
-  runApp(new PerguntaApp());
+  runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
-  @override
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+
+    print(_perguntaSelecionada);
+  }
+
+  @override //método build é obrigatório
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
-      home: Text('Olá Flutter'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Questao(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+              child: Text('Respota 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Respota 2'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Respota 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
